8a3231680b763acfac7f1f9537a288d21a6d6e65
zeyad03 <122759970+zeyad03@users.noreply.github.com>
2024-05-08 23:27:18 +0100

    add parser files

---
+++ Parser Files/parser.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "lexer.h"
+#include "parser.h"
+
+
+// you can declare prototypes of parser functions below
+
+
+
+
+int InitParser (char* file_name)
+{
+
+	return 1;
+}
+
+ParserInfo Parse ()
+{
+	ParserInfo pi;
+
+	// implement the function
+
+
+	pi.er = none;
+	return pi;
+}
+
+
+int StopParser ()
+{
+	return 1;
+}
+
+#ifndef TEST_PARSER
+int main ()
+{
+
+	return 1;
+}
+#endif
